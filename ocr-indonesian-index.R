4021cb2dfa1f93953b097d3ea20a7fb7c361e105
Enggano <engganolang@gmail.com>
2025-03-18 18:18:08 +0800

    Update code to edit first page.

--- ocr-indonesian-index.R
+++ ocr-indonesian-index.R
@@ -15,3 +15,19 @@ pdfs
 # unlink(dir(pattern = "\\.png$"))
 # saveRDS(txts, file = "ocr/indonesian-index.rds")
 ## ==== the preceding codes have been executed ==== ##
+
+idn <- readRDS("ocr/indonesian-index.rds")
+
+# first page
+txt <- idn[[1]] |> 
+str_split("\\n") |> 
+unlist() |> 
+str_replace("^A ", "") |>
+str_subset("^(b\\]|3$)", negate = TRUE) |>
+(\(x) x[-c(1:4)])() |>
+(\(x) x[nzchar(x)])()
+
+txt1 <- txt |> 
+str_replace("(?<=\\,)(\\s[0-9]+)\\s([^0-9])", "\\1\n\\2") |>
+str_split("\\n") |>
+unlist()
