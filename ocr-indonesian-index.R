da9eb604b7312d7ebee9b97157718ab351efc657
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-03-18 19:45:26 +0800

    Update editing codes.

--- ocr-indonesian-index.R
+++ ocr-indonesian-index.R
@@ -1,3 +1,6 @@
+# Gede Primahadi W. Rajeg (2025)
+# CIRHSS, CompLexico, Udayana University
+
 library(tidyverse)
 library(pdftools)
 library(tesseract)
@@ -5,6 +8,8 @@ library(tesseract)
 pdfs <- dir("pdf", full.names = TRUE)
 pdfs <- str_subset(pdfs, "indonesian-index")
 pdfs
+
+# dir.create("../digitised-holle-list/data/indonesian-index")
 
 # Indonesian Index ======
 ## ==== the following codes have been executed ==== ##
@@ -18,16 +23,61 @@ pdfs
 
 idn <- readRDS("ocr/indonesian-index.rds")
 
-# first page
-txt <- idn[[1]] |> 
+
+# first page =====
+
+idn[[1]] |> 
 str_split("\\n") |> 
 unlist() |> 
 str_replace("^A ", "") |>
 str_subset("^(b\\]|3$)", negate = TRUE) |>
 (\(x) x[-c(1:4)])() |>
-(\(x) x[nzchar(x)])()
-
-txt1 <- txt |> 
+(\(x) x[nzchar(x)])() |> 
 str_replace("(?<=\\,)(\\s[0-9]+)\\s([^0-9])", "\\1\n\\2") |>
 str_split("\\n") |>
-unlist()
+unlist() |> 
+write_lines("../digitised-holle-list/data/indonesian-index/sheet-01.txt")
+
+
+# second page =====
+
+idn[[2]] |>
+str_replace("^19\\n\\nB\\s", "") |>
+str_split("\\n") |>
+unlist() |>
+(\(x) x[nzchar(x)])() |>
+str_replace("(?<=\\,)(\\s[0-9]+)\\s([^0-9])", "\\1\n\\2") |>
+str_split("\\n") |>
+unlist() |> 
+write_lines("../digitised-holle-list/data/indonesian-index/sheet-02.txt")
+
+
+# third page =====
+
+idn[[3]] |>
+str_replace("^80\\n", "") |>
+str_split("\\n") |>
+unlist() |>
+str_replace("berbicara dengan artikulasi ", "") |>
+str_replace("^(uvular)", "berbicara dengan artikulasi \\1") |>
+(\(x) x[nzchar(x)])() |>
+str_replace("(?<=\\,)(\\s[0-9]+)\\s([^0-9])", "\\1\n\\2") |>
+str_split("\\n") |>
+unlist() |>
+write_lines("../digitised-holle-list/data/indonesian-index/sheet-03.txt")
+
+
+# fourth page =====
+
+idn[[4]] |>
+str_replace("^81\\n", "") |>
+str_split("\\n") |>
+unlist() |> 
+str_replace("(?<=88)\\sC$", "") |>
+str_replace("e210", "210") |>
+str_replace("(?<=79\\s)_\\s(?=burung)", "") |>
+(\(x) x[nzchar(x)])() |>
+str_replace("(?<=\\,)(\\s[0-9]+)\\s([^0-9])", "\\1\n\\2") |>
+str_split("\\n") |>
+unlist() |>
+write_lines("../digitised-holle-list/data/indonesian-index/sheet-04.txt")
