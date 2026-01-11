7763c3a71f24c581e3d8945f5cc01a354c091131
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2026-01-11 17:40:34 +0800

    Update code to Resolve Holle-List/nias1905-holle-list#1

--- processing-nias1905-googlespreadsheet.R
+++ processing-nias1905-googlespreadsheet.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(googlesheets4)
 
 source("merge-NBL-call.R") # run codes to retrieve the NBL and NBL's Concepticon mapping
+source("gloss-English-and-Indonesian-added-to-unsp-Dutch.R")
 
 maindb <- googlesheets4::read_sheet(ss = "https://docs.google.com/spreadsheets/d/1P-JontDvH4MjKZ4pdqxthjTovSajJLKX6y2rtpuO5sc/edit?usp=sharing",
                                           col_types = c("cccccccccccicccccccccccccccllcccccccccccccc"),
@@ -39,12 +40,26 @@ nias1905_tb <- nias1905 |>
   relocate(Swadesh, .after = v1931) |> 
   relocate(Swadesh_orig, .after = Swadesh) |> 
   relocate(remark, .after = Swadesh_orig) |> 
+  # If there is empty translation from the original English (e.g., due to Dutch ... <unsp.>), get the English and Indonesian from dv and de columns
+  # mutate(English_add = if_else(English == "" & str_detect(Dutch, "(unsp\\.)"), de, ""),
+  #        Indonesian_add = if_else(Indonesian == "" & str_detect(Dutch, "(unsp\\.)"), dv, "")) |> 
   # NFD transformation so that the diacritics can be searched via Keyman combining keystroke
   mutate(across(matches("(^Forms$|^remark$|^nt_)"), ~stringi::stri_trans_nfd(.)))
 
 nias1905_tb_out <- nias1905_tb |> 
-  select(cats, 2:13, matches("^nt_"), matches("oncept(icon)?_")) |> 
-  select(!matches("(tapah|lekon|simalur)"))
+  select(cats, 2:13, 
+         #English_add, 
+         #Indonesian_add, 
+         matches("^nt_"), matches("oncept(icon)?_")) |> 
+  select(!matches("(tapah|lekon|simalur)")) |> 
+  left_join(nogloss3) |> # add the gloss for \ and - IDs
+  mutate(English = if_else(!is.na(English3), 
+                           English3, 
+                           English)) |>
+  mutate(Indonesian = if_else(!is.na(Indonesian3), 
+                              Indonesian3, 
+                              Indonesian)) |> 
+  select(-English3, -Indonesian3)
 nias1905_tb_out
 nias1905_tb_out |> write_tsv("data-output/nias1905.tsv")
 nias1905_tb_out |> write_tsv("../nias1905-holle-list/raw/nias1905.tsv")
