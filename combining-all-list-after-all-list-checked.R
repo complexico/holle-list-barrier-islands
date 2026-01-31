41e6f8a779a9d222ea49b54e237a1fe70ab1e3f5
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2026-02-01 00:22:32 +0800

    Add codes to check entries with multiple forms separated by commas, or semicolons, or forward slash, in a cell across all languages to re-address #18-comment-3828703667.

--- combining-all-list-after-all-list-checked.R
+++ combining-all-list-after-all-list-checked.R
@@ -9,5 +9,37 @@ db <- map(wl_files, read_tsv) |>
 
 names(db) <- lang_name
 
+factor_order <- c("lekon", "tapah", "simalur", "seumalur1912", "sigule_salang1912", "salang_sigule1920", "mentawai_nd", "mentawai1933", "nias1905", "nias1911")
 
-db |> map(colnames)
+# test filtering data
+db |> 
+  map(~filter(., str_detect(English, "fire"))) |> 
+  map(~select(., lang_name, Index, Forms, Dutch, English, Indonesian)) |> 
+  list_rbind() |> 
+  mutate(lang_name = factor(lang_name, levels = factor_order)) |> 
+  arrange(lang_name, Forms)
+
+# test checking multiple forms in a cell
+db |> 
+  map(~filter(., str_detect(Forms, ","))) |> 
+  map(~select(., lang_name, Index, Forms, Dutch, English, Indonesian)) |> 
+  list_rbind() |> 
+  mutate(lang_name = factor(lang_name, levels = factor_order)) |> 
+  arrange(lang_name, Forms)
+
+db |> 
+  map(~filter(., str_detect(Forms, ","))) |> 
+  map(~select(., lang_name, Index, Forms, Dutch, English, Indonesian)) |> 
+  list_rbind() |> 
+  mutate(lang_name = factor(lang_name, levels = factor_order)) |> 
+  arrange(lang_name, Forms) |> 
+  count(lang_name)
+
+# A tibble: 5 × 2
+# lang_name             n
+# <fct>             <int>
+#   1 tapah                47
+# 2 seumalur1912          8
+# 3 sigule_salang1912    10
+# 4 mentawai_nd           4
+# 5 nias1911              5
