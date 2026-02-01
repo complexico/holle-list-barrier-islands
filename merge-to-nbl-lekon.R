ba1a3bdd9a1d29e590c2baffec2b01c7224895b5
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2026-02-01 10:43:47 +0800

    Lekon: update raw list and merging code to split multiple items in a cell in relation to #18 and https://github.com/complexico/holle-list-barrier-islands/issues/18#issuecomment-3830143306, and add list_type column.

--- merge-to-nbl-lekon.R
+++ merge-to-nbl-lekon.R
@@ -116,6 +116,7 @@ tb <- tb |>
                          str_replace(Forms, ",\\sbòlêm ònding", ""),
                          Forms)) |> 
   # split multiple forms in Forms and nt_form
+  separate_longer_delim(Forms, "/") |> 
   mutate(Forms = str_split(Forms, ", ")) |> 
   unnest_longer(col = Forms) |> 
   mutate(nt_form = str_split(nt_form, ", ")) |> 
@@ -133,6 +134,23 @@ tb <- tb |>
   mutate(Indonesian = if_else(!is.na(Indonesian3), 
                               Indonesian3, 
                               Indonesian)) |> 
-  select(-English3, -Indonesian3)
+  select(-English3, -Indonesian3) |> 
+  # make FormsAll into Forms and change Forms into FormsOrig, which store the original forms in the list that can contain empty forms due to reference to the notes.
+  rename(FormsOrig = Forms,
+         Forms = FormsAll) |> 
+  relocate(Forms, .after = Index) |> 
+  relocate(FormsOrig, .after = concept_url) |> 
+  distinct() |> 
+  # add codes to determine the types of the list based on the Index
+  mutate(list_type = "NBL",
+         list_type = if_else(Index %in% holle_1904_1911$Index,
+                             "added_list_1904_1911",
+                             list_type),
+         list_type = if_else(Index %in% holle_1931$Index,
+                             "added_list_1931",
+                             list_type),
+         list_type = if_else(str_detect(Index, "^add_"),
+                             "added_data",
+                             list_type))
 
 write_tsv(tb, "data-output/lekon_tb.tsv")
